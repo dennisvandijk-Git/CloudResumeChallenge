106bb506a42725c64992dbcf604a1b3df71ed95c
dennisvandijk-Git <dennisvandijk91@live.nl>
2024-10-03 07:01:37 +0200

    Added fetch-timeout

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const visitsCount = document.getElementById("visits-count");
 
 fetch(
-  "https://visitor-counter-api-http-trigger.azurewebsites.net/api/access-key?"
+  "https://visitor-counter-api-http-trigger.azurewebsites.net/api/access-key?",
+  {
+    signal: AbortSignal.timeout(5000),
+  }
 )
   .then((response) => response.json())
   .then((data) => {
