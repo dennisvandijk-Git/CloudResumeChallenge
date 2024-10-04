c086eb0344acbd8a7d7500482a814c6243526bb1
dennisvandijk-Git <dennisvandijk91@live.nl>
2024-10-04 20:32:49 +0200

    styling-update

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const visitsCount = document.getElementById("visits-count");
 
 fetch(
-  "https://visitor-counter-api-http-trigger.azurewebsites.net/api/access-key?",
-  {
-    signal: AbortSignal.timeout(5000),
-  }
+  "https://visitor-counter-api-http-trigger.azurewebsites.net/api/http_trigger_visitor_counter?"
 )
   .then((response) => response.json())
   .then((data) => {
-    visitsCount.textContent = data[0].visitsCount;
+    visitsCount.textContent = data;
   });
